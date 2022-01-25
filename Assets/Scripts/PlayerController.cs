b5939eb9aa6cb728029adc315f24c06de4e63c43
Stefan Alfredsson <stefan@argentrealms.com>
2022-01-25 13:02:16 +0100

    Added moving and jumping to PlayerController.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody myRigidbody;
+    public float moveSpeed = 5f;
+    public float jumpForce = 500f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Get move input
+        var moveInput = Input.GetAxis("Horizontal");
+        //Print move input in the console
+        //Debug.Log("Our move input:" + moveInput);
+
+        //Apply moveSpeed to rigidbody
+        myRigidbody.velocity = new Vector3(moveInput * moveSpeed, myRigidbody.velocity.y, 0);
+
+        //Get jump input
+        var jumpInput = Input.GetKeyDown(KeyCode.Space);
+
+        //If we pressed the jump button: then jump
+        if (jumpInput == true)
+        {
+            myRigidbody.AddForce(0, jumpForce, 0);
+        }
     }
 }
