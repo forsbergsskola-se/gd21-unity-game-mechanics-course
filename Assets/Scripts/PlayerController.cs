30f6493f9f9ee5015fe47578ef27ff3dfac60bd7
Stefan Alfredsson <stefan@argentrealms.com>
2022-01-26 10:40:01 +0100

    Added ground checking to jumping.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public Rigidbody myRigidbody;
     public float moveSpeed = 5f;
     public float jumpForce = 500f;
+    public float groundCheckDistance = 1f;
+    public float groundCheckSphereRadius = 0.5f;
 
     // Update is called once per frame
     void Update()
@@ -22,10 +24,32 @@ public class PlayerController : MonoBehaviour
         //Get jump input
         var jumpInput = Input.GetKeyDown(KeyCode.Space);
 
+        //Check if we're grounded, using a raycast.
+        //Vector3.down: down in world space.
+        //-transform.up: down in the GameObject's local space.
+        // var isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance);
+
+        //Ground checking using sphere cast. Think of it as a sphere being moved along a ray, we hit anything that the sphere touches.
+        var sphereCastRay = new Ray(transform.position, Vector3.down);
+        var isGrounded = Physics.SphereCast(sphereCastRay, groundCheckSphereRadius, groundCheckDistance);
+
+        //Draw a ray in the editor, only for visualization.
+        // Debug.DrawRay(transform.position, Vector3.down * groundCheckDistance, Color.cyan);
+
         //If we pressed the jump button: then jump
-        if (jumpInput == true)
+        if (jumpInput == true && isGrounded == true)
         {
+            // Debug.Log("Jump");
             myRigidbody.AddForce(0, jumpForce, 0);
         }
     }
+
+    //Example of two methods with the same name but with different signatures.
+    // private void TestMethod(int one, float two)
+    // {
+    // }
+    //
+    // private void TestMethod(int one, float two, int three)
+    // {
+    // }
 }
